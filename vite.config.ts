58aece89e756a9772232991f911e335ab5268ec8
ntaokshaiyoa-afk <ntaokshaiyoa@gmail.com>
2026-03-01 20:44:07 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/stest/',   // ← GitHub Pages用（リポジトリ名）
-  server: {
-    port: 3000,
-  },
-});
+  base: '/stest/'
+})
