e9574b6c45a954855961b38b3af955b168f90c71
sanoya <ntaokshaiyoa@gmail.com>
2026-03-01 22:25:16 +0900

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,33 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-plugins: [
-  react(),
-  VitePWA({
-    registerType: 'autoUpdate',
-    manifest: {
-      name: 'ビジネス基礎力チェック',
-      short_name: 'BizQuiz',
-      start_url: '/stest/',
-      display: 'standalone',
-      background_color: '#ffffff',
-      theme_color: '#ffffff',
-      icons: [
-        {
-          src: '/stest/pwa-192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/stest/pwa-512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    }
-  })
-]
+export default defineConfig({
+  base: '/stest/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'ビジネス基礎力チェック',
+        short_name: 'BizQuiz',
+        start_url: '/stest/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
